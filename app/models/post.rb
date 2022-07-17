737a32dadb9e8cb8503177e87b2caa0585e7cddc
gerry <hkggerry@gmail.com>
2022-07-17 12:44:24 -0700

    Completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+    # validate :true_facts
+
+    # def true_facts
+    #     unless title.match?(["Won't Believe", "Secret", "Top [number]", "Guess"])
+    #         errors.add(:title, "No Match!")
+    #     end
+    # end
 end
